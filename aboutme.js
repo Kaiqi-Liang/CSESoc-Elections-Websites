e8b78db9229d6cb466167371a187c519cfdf4067
Kaiqi <kaiqi.liang9989@gmail.com>
2020-02-14 13:33:21 +0800

    Change description

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,4 @@
-import csesoc from 'text.js'
+//import csesoc from 'text.js'
 const aboutme = document.getElementById('aboutme')
 const info = document.getElementById('info')
 document.getElementById('dropdown').addEventListener('click', (event) => {
@@ -8,7 +8,7 @@ document.getElementById('dropdown').addEventListener('click', (event) => {
     } else {
         info.style.display = 'none'
         const p = document.createElement('p')
-            p.innerText = csesoc
-            aboutme.appendChild(p)
+        p.innerText = "Hi welcome to my website, I am currently in my penultimate year of Computer Science at UNSW. Apart from programming my life also involves music and sports. I am an active person enjoying all sorts of sports, as well as my other hobby being singing and piano. Javascript is my favourite language as I'm writing it right now. Python is my least favrouite but it is also one of my best ironically."
+        aboutme.appendChild(p)
     }
 })
